833651c07b2c4e0df6fd4bf082a2f48e866bd92c
jakub.coufal <jakub.coufal@wandera.com>
2017-11-22 13:42:30 +0100

    Fixed cluster teardown

--- aws/cluster/main.tf
+++ aws/cluster/main.tf
@@ -14,7 +14,7 @@ EOF
   // On destroy, remove the cluster first, if it exists
   provisioner "local-exec" {
     when    = "destroy"
-    command = "(test -z \"$(kops get cluster | grep ${var.cluster-name})\" ) || kops --state=s3://${var.kops-state-bucket} delete cluster --yes ${var.cluster-name}"
+    command = "(test -z \"$(kops --state=s3://${var.kops-state-bucket} get cluster | grep ${var.cluster-name})\" ) || kops --state=s3://${var.kops-state-bucket} delete cluster --yes ${var.cluster-name}"
   }
 
   depends_on = ["aws_route53_record.cluster-root", "aws_vpc.main"]
