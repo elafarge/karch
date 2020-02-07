2c264c0a402c2c2da4e09b0716f482138e28d20c
Petr Cerny <petr.cerny@wandera.com>
2020-02-07 12:13:13 +0100

    Decrease TTL of NXDOMAIN responses for cluster domain.

--- aws/cluster/route53.tf
+++ aws/cluster/route53.tf
@@ -2,6 +2,15 @@
  * Cluster public subdomain configuration
  */
 
+locals {
+  dns_soa_hostmaster   = "awsdns-hostmaster.amazon.com"
+  dns_soa_refresh_time = 7200
+  dns_soa_retry_time   = 900
+  dns_soa_expire_time  = 1209600
+  dns_soa_negative_ttl = 60
+  dns_soa_appendix     = "${local.dns_soa_hostmaster}. 1 ${local.dns_soa_refresh_time} ${local.dns_soa_retry_time} ${local.dns_soa_expire_time} ${local.dns_soa_negative_ttl}"
+}
+
 resource "aws_route53_zone" "cluster" {
   name          = "${var.cluster-name}"
   force_destroy = true
@@ -21,4 +30,14 @@ resource "aws_route53_record" "cluster-root" {
     "${aws_route53_zone.cluster.name_servers.2}",
     "${aws_route53_zone.cluster.name_servers.3}",
   ]
+}
+
+resource "aws_route53_record" "cluster-soa" {
+  zone_id = "${aws_route53_zone.cluster.id}"
+  name    = "${aws_route53_zone.cluster.name}"
+  type    = "SOA"
+  ttl     = "60"
+  records = ["${aws_route53_zone.cluster.name_servers.0}. ${local.dns_soa_appendix}"]
+
+  allow_overwrite = true
 }
