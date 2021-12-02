61ab1733e4feff5e7f2a45a39cc7a47f66cb1b37
Petr Černý <petr.cerny@wandera.com>
2021-12-02 15:17:48 +0100

    Use simpler datasource for IGs to avoid incompatible change in AWS provider v3.68

--- aws/mig/data.tf
+++ aws/mig/data.tf
@@ -1,9 +1,4 @@
-data "aws_autoscaling_groups" "ig" {
-  filter {
-    name = "auto-scaling-group"
-
-    // The second value simply is a way to make sure this data source will
-    // only be provisioned once these subnets have been created by kops
-    values = ["${var.name}.${var.cluster-name}", null_resource.ig-update.id]
-  }
+data "aws_autoscaling_group" "ig" {
+  name       = "${var.name}.${var.cluster-name}"
+  depends_on = [null_resource.ig-update]
 }
